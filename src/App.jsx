2c7456ac5da0b0f1c716936b40c1771837e01664
muh11 <naufalaswinana@gmail.com>
2024-04-06 00:16:34 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import MyLink from "./components/MyLink";
+import profile from './assets/foto_muh.jpeg';
+
+function App() {
+  return (
+    <body className="min-h-screen grid bg-[#141414] place-items-center">
+      <div className="bg-[#1f1f1f] size-auto p-10 flex-col rounded-lg">
+        <div className="text-center">
+          <div className="flex justify-center">
+          <img src={profile} alt="foto-profile" className="rounded-full size-28"/>
+          </div>
+          <h1 className="text-white font-bold text-2xl pt-6"> Naufal Muhammad</h1>
+          <h1 className="text-lime-400 font-medium text-lg">
+            Depok, Indonesia
+          </h1>
+          <div className="text-normal italic text-white pt-7 py-5">
+            "Front-end Developer and avid reader."
+          </div>
+          <div className="grid gap-4">
+          <MyLink href="https://github.com/NaufalMuh11">Github</MyLink>
+          <MyLink href="https://www.linkedin.com/in/naufal-muhammad-a36839247">LinkedIn</MyLink>
+          <MyLink href="https://twitter.com/Paannih1">Twitter</MyLink>
+          <MyLink href="https://www.instagram.com/naufalmuh.11/">Instagram</MyLink>
+          <MyLink href="">Frontend Mentor</MyLink>
+          </div>
+        </div>
+        <div class="w-full flex flex-col gap-4"></div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
---
+++ src/components/MyLink.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyLink ({ href, children }) {
+  return (
+    <a href={href} target="_blank" className="bg-[#333] hover:bg-slate-50 text-slate-50 hover:text-[#333] text-base font-semibold py-4 rounded-lg hover:scale-105 active:scale-90 duration-300 w-96">
+      {children}
+    </a>
+  );
+};
+
+export default MyLink;
