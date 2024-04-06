6db1594df5f18ed4cccab3c5203b04e7ae3558ab
muh11 <naufalaswinana@gmail.com>
2024-04-06 10:33:48 +0700

    fixed

--- src/components/MyLink.jsx
+++ src/components/MyLink.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MyLink ({ href, children }) {
   return (
-    <a href={href} target="_blank" className="bg-[#333] hover:bg-slate-50 text-slate-50 hover:text-[#333] text-sm font-semibold py-4 rounded-lg hover:scale-105 active:scale-90 duration-300 w-94">
+    <a href={href} target="_blank" className="bg-[#333] hover:bg-slate-50 text-slate-50 hover:text-[#333] text-sm font-semibold py-3 rounded-lg hover:scale-105 active:scale-90 duration-300 w-94">
       {children}
     </a>
   );
