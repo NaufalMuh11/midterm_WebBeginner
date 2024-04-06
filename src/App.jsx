5eb27e40f2a0fb3abcae2a04a4d37504d0ab1dac
muh11 <naufalaswinana@gmail.com>
2024-04-06 10:18:32 +0700

    height

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import profile from './assets/foto_muh.jpeg';
 function App() {
   return (
     <div className="min-h-screen bg-[#141414] flex justify-center items-center">
-    <div className="bg-[#1f1f1f] w-[80%] h-[60%] sm:w-full max-w-[400px] p-9 flex flex-col rounded-lg">
+    <div className="bg-[#1f1f1f] w-[80%] h-[60%] max-w-[400px] p-9 flex flex-col rounded-lg">
       <div className="text-center">
         <div className="flex justify-center">
           <img src={profile} alt="foto-profile" className="rounded-full size-24"/>
