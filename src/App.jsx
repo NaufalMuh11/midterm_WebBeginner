6a546b112d765480b8cf2a666395e56049fa982f
muh11 <naufalaswinana@gmail.com>
2024-04-06 09:58:26 +0700

    rapihin height

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import profile from './assets/foto_muh.jpeg';
 
 function App() {
   return (
-    <body className="mw-full grid bg-[#141414] place-items-center">
-      <div className="bg-[#1f1f1f] size-auto p-10 flex-col rounded-lg">
-        <div className="text-center">
-          <div className="flex justify-center">
-          <img src={profile} alt="foto-profile" className="rounded-full size-28"/>
+    <body className="min-h-screen bg-[#141414] flex justify-center items-center">
+    <div className="bg-[#1f1f1f] w-[80%] h-[60%] sm:w-full max-w-[400px] p-9 flex flex-col rounded-lg">
+      <div className="text-center">
+        <div className="flex justify-center">
+          <img src={profile} alt="foto-profile" className="rounded-full size-24"/>
           </div>
-          <h1 className="text-white font-bold text-2xl pt-6"> Naufal Muhammad</h1>
-          <h1 className="text-lime-400 font-medium text-lg">
+          <h1 className="text-white font-bold text-xl pt-6"> Naufal Muhammad</h1>
+          <h1 className="text-lime-400 font-medium text-l">
             Depok, Indonesia
           </h1>
-          <div className="text-normal italic text-white pt-7 py-5">
+          <div className="text- italic text-white pt-7 py-5">
             "Front-end Developer and avid reader."
           </div>
           <div className="grid gap-4">
